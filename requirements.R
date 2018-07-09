25644acf34666a65e46b1314a27b32e518559db8
Jesus <japbogado@gmail.com>
2018-07-09 08:34:24 -0400

    add a requirements script

---
+++ requirements.R
@@ -0,0 +1,18 @@
+packs <- c(
+  "dplyr",
+  "DT",
+  "glue",
+  "highcharter",
+  "import",
+  "janitor",
+  "lubridate",
+  "magrittr",
+  "purrr",
+  "readr",
+  "readxl",
+  "shiny",
+  "shinydashboard",
+  "tidyr"
+)
+
+install.packages(packs)
