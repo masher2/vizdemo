2d42c4796bafd96339bc1aae52b0d8d13f8bad27
Jesus Perez <japbogado@gmail.com>
2018-12-07 12:09:09 -0400

    Prepare for heroku deployment.

---
+++ init.R
@@ -0,0 +1,26 @@
+packs <- c(
+  "dplyr",
+  "DT",
+  "glue",
+  "highcharter",
+  "import",
+  "janitor",
+  "lubridate",
+  "magrittr",
+  "purrr",
+  "readr",
+  "readxl",
+  "shiny",
+  "shinydashboard",
+  "tidyr"
+)
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(packs, install_if_missing))
+
+source("/app/cleaning_script.R")
